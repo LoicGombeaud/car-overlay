7c90ae75cc8892e0a0ba6dae88910fccc3d6b923
Loïc Gombeaud <loic.antoinegombeaud@gmail.com>
2023-02-25 13:05:38 +0100

    feat(front): Add static front page

---
+++ front/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import "./scss/styles.scss"
+</script>
+
+<template>
+  <div class="container">
+    <div class="row px-2">
+      <div class="col-lg-3"></div>
+      <div class="col-lg-6">
+        <div class="display-5">Le point de vue de l'automobiliste</div>
+        <img class="img-fluid my-3" src="./assets/before-after.webp">
+        <p>
+          De plus en plus de personnes à vélo filment leurs trajets, afin de rendre compte des conséquences désagréables, voire dangereuses, de la cohabitation avec certains automobilistes.
+        </p>
+        <p>
+          Quand ces vidéos sont mises en ligne sur les réseaux sociaux, nombreux sont les non-pratiquant.e.s du vélo à fustiger la personne à vélo, l'accablant de tous les maux, et allant parfois jusqu'à inventer des infractions imaginaires.
+        </p>
+        <p>
+          Mais quelles seraient les réactions si, à situation équivalente, la vidéo avait été filmée depuis une voiture plutôt que sur un vélo ?
+        </p>
+        <p>
+          Ce projet aide à répondre à cette question, en permettant d'ajouter facilement les contours d'un habitacle de voiture sur une vidéo, en 3 étapes seulement :
+          <ol>
+            <li>upload de la vidéo originale</li>
+            <li>attente pendant le traitement (de quelques secondes à une minute)</li>
+            <li>téléchargmement de la vidéo modifiée</li>
+          </ol>
+        </p>
+      </div>
+      <div class="col-lg-3"></div>
+    </div>
+  </div>
+</template>
